d0ffb72741e90205ad986f28a6cfe8f12f698d73
aylaylay <fortblox@gmail.com>
2019-08-08 03:35:35 +0100

    rename

--- donut-codegen/Properties/AssemblyInfo.cs
+++ donut-codegen/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("p3dcppgen")]
+[assembly: AssemblyTitle("donut-codegen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("p3dcppgen")]
+[assembly: AssemblyProduct("donut-codegen")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
